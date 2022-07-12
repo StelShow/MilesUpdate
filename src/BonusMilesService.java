d39ca963f6e557f60baa18e58af833ab9571a785
denmoz21@ya.ru <denmoz21@ya.ru>
2022-07-12 17:35:39 +0300

    Up

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int miles = price / 20;
+        if (miles <= 0) {
+            System.out.println("Стоимость билета не может быть отрицательной или равна нулю");
+        }
+        return miles;
+    }
+}
+
